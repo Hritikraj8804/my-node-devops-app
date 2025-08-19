ffa3760b66d33c208dc87a9753b3941e75885195
Hritik Raj <hritikraj8804@gmail.com>
2025-08-19 20:40:53 +0530

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello from my Node.js DevOps App');
+  res.send('Hello from my Node DevOps App');
 });
 
 app.listen(port, () => {
